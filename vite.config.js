089f4aa0d8f178b42e55d7abe8e1a6cace2b0330
JhonEG-17 <jhonataneg@outlook.com>
2026-01-23 23:02:01 -0600

    fix: No name was provided for external module "lit" in "output.globals" – guessing "lit".

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,20 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     lib: {
-      // El punto de entrada de tu librería (donde exportas todo)
       entry: 'src/index.js',
-      // El nombre de la variable global si alguien lo usa sin módulos
+      
       name: 'SemaUI',
-      // Cómo se llamarán los archivos generados en /dist
+      
       fileName: (format) => `sema-ui.${format}.js`
     },
     rollupOptions: {
-      // IMPORTANTE: Le decimos a Vite que NO meta 'lit' dentro del paquete.
-      // Así evitamos que si el usuario ya usa Lit, se descargue dos veces.
-      external: /^lit/
+      external: /^lit/, 
+      
+      output: {
+        globals: {
+          lit: 'lit'
+        }
+      }
     }
   }
 });
